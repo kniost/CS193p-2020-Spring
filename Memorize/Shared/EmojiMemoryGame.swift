bf8dc181ceb2b8b7b95c167de700e0308f54e854
kniost <kniost33@gmail.com>
2020-08-27 13:59:58 +0800

    Lecture 2 MVVM and the Swift Type System

---
+++ Memorize/Shared/EmojiMemoryGame.swift
@@ -0,0 +1,34 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Andrew on 2020/8/27.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: [MemoryGame<String>.Card] {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+    
+    
+}
+
---
+++ Memorize/Shared/MemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Andrew on 2020/8/27.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: [Card]
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = [Card]()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex * 2))
+            cards.append(Card(content: content, id: pairIndex * 2 + 1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
